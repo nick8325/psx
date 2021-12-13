1a6874cabf0a56b8c9991627c89385d87eb1ed2b
Nick Smallbone <nick@smallbone.se>
2021-12-13 16:24:59 +0100

    A bit more progress

--- src/cpu.zig
+++ src/cpu.zig
@@ -4,6 +4,8 @@ const utils = @import("utils.zig");
 
 /// A register is a 5-bit integer.
 pub const Register = struct { val: u5 };
+/// A coprocessor register.
+pub const CoRegister = struct { val: u5 };
 
 /// A decoded instruction.
 pub const Instruction = union(enum) {
@@ -12,21 +14,21 @@ pub const Instruction = union(enum) {
     /// Three-register instructions.
     reg: struct {
         op: RegisterOp,
-        source1: Register,
-        source2: Register,
+        src1: Register,
+        src2: Register,
         dest: Register
     },
     /// Shift instructions.
     shift: struct {
         op: ShiftOp,
-        source: Register,
+        src: Register,
         dest: Register,
         amount: u5
     },
     /// Instructions with an immediate operand.
     imm: struct {
         op: ImmediateOp,
-        source: Register,
+        src: Register,
         dest: Register,
         imm: u16,
 
@@ -53,7 +55,17 @@ pub const Instruction = union(enum) {
             return (pc & 0xf0000000) | (@as(u32, self.target) << 2);
         }
     },
-
+    /// Move to/from coprocessor.
+    mtc: struct {
+        cop: u2,
+        src: Register,
+        dest: CoRegister
+    },
+    mfc: struct {
+        cop: u2,
+        src: CoRegister,
+        dest: Register
+    },
     pub fn format(
         self: Instruction,
         comptime fmt: []const u8,
@@ -65,10 +77,12 @@ pub const Instruction = union(enum) {
 
         switch (self) {
             .nop => try writer.print("NOP", .{}),
-            .reg => |info| try writer.print("{s} r{}, r{}, r{}", .{@tagName(info.op), info.dest.val, info.source1.val, info.source2.val}),
-            .shift => |info| try writer.print("{s} r{}, r{}, ${}", .{@tagName(info.op), info.dest.val, info.source.val, info.amount}),
-            .imm => |info| try writer.print("{s} r{}, r{}, $0x{x}", .{@tagName(info.op), info.dest.val, info.source.val, info.imm}),
-            .jump => |info| try writer.print("{s} $0x{x}", .{@tagName(info.op), info.target})
+            .reg => |info| try writer.print("{s} r{}, r{}, r{}", .{@tagName(info.op), info.dest.val, info.src1.val, info.src2.val}),
+            .shift => |info| try writer.print("{s} r{}, r{}, ${}", .{@tagName(info.op), info.dest.val, info.src.val, info.amount}),
+            .imm => |info| try writer.print("{s} r{}, r{}, $0x{x}", .{@tagName(info.op), info.dest.val, info.src.val, info.imm}),
+            .jump => |info| try writer.print("{s} $0x{x}", .{@tagName(info.op), info.target}),
+            .mtc => |info| try writer.print("MTC{} c{}, r{}", .{info.cop, info.dest.val, info.src.val}),
+            .mfc => |info| try writer.print("MFC{} r{}, c{}", .{info.cop, info.dest.val, info.src.val})
         }
     }
 
@@ -89,7 +103,9 @@ pub const ImmediateOp = enum(u6) {
     LUI = 0b001111,
     ORI = 0b001101,
     ADDIU = 0b001001,
-    SW = 0b101011
+    SW = 0b101011,
+    BNE = 0b000101,
+    ADDI = 0b001000
 };
 
 /// List of jump-type instructions.
@@ -126,8 +142,8 @@ pub fn decode(instr: u32) !Instruction {
 
             return Instruction{.reg = .{
                 .op = op,
-                .source1 = rs,
-                .source2 = rt,
+                .src1 = rs,
+                .src2 = rt,
                 .dest = rd
             }};
         } else if (utils.intToEnum(ShiftOp, funct)) |op| {
@@ -138,7 +154,7 @@ pub fn decode(instr: u32) !Instruction {
 
             return Instruction{.shift = .{
                 .op = op,
-                .source = rt,
+                .src = rt,
                 .dest = rd,
                 .amount = shift
             }};
@@ -154,7 +170,7 @@ pub fn decode(instr: u32) !Instruction {
 
         return Instruction{.imm = .{
             .op = op,
-            .source = rs,
+            .src = rs,
             .dest = rt,
             .imm = imm
         }};
@@ -163,11 +179,36 @@ pub fn decode(instr: u32) !Instruction {
             .op = op,
             .target = target
         }};
+    } else if ((opcode & 0b111100) == 0b010000) {
+        // COP instruction.
+        const cop = @intCast(u2, opcode & 0b11);
+        if (rs.val == 0b00100 and shift == 0 and funct == 0) {
+            return Instruction{.mtc = .{
+                .cop = cop,
+                .src = rt,
+                .dest = .{.val = rd.val}
+            }};
+        } else if (rs.val == 0 and shift == 0 and funct == 0) {
+            return Instruction{.mfc = .{
+                .cop = cop,
+                .src = .{.val = rd.val},
+                .dest = rt
+            }};
+        } else {
+            std.log.err("unknown COP instruction in {x}", .{instr});
+            return error.UnknownInstruction;
+        }
     } else {
         std.log.err("unknown opcode {x} in {x}", .{opcode, instr});
         return error.UnknownInstruction;
     }
 }
+
+/// Errors returned during execution
+pub const ExecutionError = error {
+    InvalidCOP,
+    Overflow
+};
 
 /// The state of the CPU.
 pub const CPU = struct {
@@ -179,12 +220,15 @@ pub const CPU = struct {
     /// Used to implement the branch delay slot.
     /// The instruction at PC will be executed after this one.
     instruction: u32,
+    /// Coprocessor 0 registers.
+    cop0: [32]u32,
 
     pub fn init(pc: u32) CPU {
         return CPU {
             .pc = pc,
             .regs = [_]u32{0} ** 32,
-            .instruction = 0
+            .instruction = 0,
+            .cop0 = [_]u32{0} ** 32,
         };
     }
 
@@ -201,6 +245,9 @@ pub const CPU = struct {
         for (self.regs) |x, i| {
             try writer.print("R{}={x} ", .{i, x});
         }
+        for (self.cop0) |x, i| {
+            try writer.print("COP0.R{}={x} ", .{i, x});
+        }
     }
 
     /// Get the value of a register.
@@ -213,6 +260,16 @@ pub const CPU = struct {
     pub fn set(self: *CPU, reg: Register, val: u32) void {
         self.regs[reg.val] = val;
         self.regs[0] = 0;
+    }
+
+    /// Get the value of a COP0 register.
+    pub fn getCOP0(self: *const CPU, reg: CoRegister) u32 {
+        return self.cop0[reg.val];
+    }
+
+    /// Set the value of a COP0 register.
+    pub fn setCOP0(self: *CPU, reg: CoRegister, val: u32) void {
+        self.cop0[reg.val] = val;
     }
 
     /// Fetch and execute the next instruction.
@@ -234,19 +291,45 @@ pub const CPU = struct {
         switch(instr) {
             .nop => {},
             .reg => |info| switch(info.op) {
-                .OR => self.set(info.dest, self.get(info.source1) | self.get(info.source2))
+                .OR => self.set(info.dest, self.get(info.src1) | self.get(info.src2))
             },
             .shift => |info| switch(info.op) {
-                .SLL => self.set(info.dest, self.get(info.source) << info.amount)
+                .SLL => self.set(info.dest, self.get(info.src) << info.amount)
             },
             .imm => |info| switch(info.op) {
                 .LUI => self.set(info.dest, info.zImm() << 16),
-                .ORI => self.set(info.dest, self.get(info.source) | info.imm),
-                .ADDIU => self.set(info.dest, self.get(info.source) +% info.sImm()),
-                .SW => try memory.write(self.get(info.source) +% info.sImm(), self.get(info.dest))
+                .ORI => self.set(info.dest, self.get(info.src) | info.imm),
+                .ADDIU => self.set(info.dest, self.get(info.src) +% info.sImm()),
+                .ADDI => {
+                    const src1 = @bitCast(i32, self.get(info.src));
+                    const src2 = @bitCast(i32, info.sImm());
+                    var dest: i32 = undefined;
+                    if (@addWithOverflow(i32, src1, src2, &dest))
+                        return error.Overflow
+                    else
+                        self.set(info.dest, @bitCast(u32, dest));
+                },
+                .SW => try memory.write(self.get(info.src) +% info.sImm(), self.get(info.dest)),
+                .BNE => {
+                    // TO DO: check if this branches to the right place
+                    if (self.get(info.src) != self.get(info.dest))
+                        self.pc +%= info.sImm() << 2;
+                }
             },
             .jump => |info| switch(info.op) {
                 .J => self.pc = (self.pc & 0xf0000000) | (@as(u32, info.target) << 2)
+            },
+            .mtc => |info| switch(info.cop) {
+                0 => self.setCOP0(info.dest, self.get(info.src)),
+                else => {
+                    std.log.err("invalid cop {} in {x}", .{info.cop, instr});
+                }
+            },
+            .mfc => |info| switch(info.cop) {
+                0 => self.set(info.dest, self.getCOP0(info.src)),
+                else => {
+                    std.log.err("invalid cop {} in {x}", .{info.cop, instr});
+                }
             }
         }
     }
@@ -274,6 +357,10 @@ pub const CPUDiff = struct {
             for (cpu1.regs) |x, i| {
                 if (x != self.cpu2.regs[i])
                     try writer.print("R{}={x}->{x} ", .{i, x, self.cpu2.regs[i]});
+            }
+            for (cpu1.cop0) |x, i| {
+                if (x != self.cpu2.cop0[i])
+                    try writer.print("COP0.R{}={x}->{x} ", .{i, x, self.cpu2.cop0[i]});
             }
         } else try self.cpu2.format(fmt, options, writer);
     }
