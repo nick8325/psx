1f62e58da1956de7ed27233fd35cb4a58e47386b
Nick Smallbone <nick@smallbone.se>
2022-01-07 20:42:30 +0100

    Make decoder more compact, and add a disassembler

--- src/cpu.nim
+++ src/cpu.nim
@@ -12,6 +12,9 @@ type
   Register* = distinct range[0..31] ## A register.
   CoRegister = distinct range[0..31] ## A coprocessor register.
 
+const
+  r0 = Register(0)
+
 func `==`*(x, y: Register): bool {.borrow.}
 
 func `$`*(x: Register): string =
@@ -131,7 +134,7 @@ func `[]`(cpu: CPU, reg: Register): uint32 =
 func `[]=`(cpu: var CPU, reg: Register, val: uint32) =
   ## Access registers by number. Takes care of ignoring writes to R0.
   cpu.registers[reg] = val
-  cpu.registers[Register(0)] = 0
+  cpu.registers[r0] = 0
 
 proc resolveAddress(cpu: CPU, address: uint32, code: bool): uint32 =
   ## Resolve a virtual address to a physical address,
@@ -181,14 +184,30 @@ func cpuDiff(cpu1: CPU, cpu2: CPU): string =
 type
   Opcode* {.pure.} = enum
     ## An instruction opcode.
-    SLL, SRL, SRA, SLLV, SRLV, SRAV, JR, JALR, SYSCALL, BREAK, MFHI, MTHI, MFLO,
-    MTLO, MULT, MULTU, DIV, DIVU, ADD, ADDU, SUB, SUBU, AND, OR, XOR, NOR, SLT,
-    SLTU, BLTZ, BGEZ, BLTZAL, BGEZAL, J, JAL, BEQ, BNE, BLEZ, BGTZ, ADDI, ADDIU,
-    SUBI, SUBIU, ANDI, ORI, XORI, LUI, LB, LH, LWL, LW, LBU, LHU, LWR, SB, SH,
-    SWL, SW, SWR, MFC0, MTC0
-
+    # Arithmetic
+    ADD, ADDI, ADDU, ADDIU, SUB, SUBU, SUBI, SUBIU,
+    DIV, DIVU, MULT, MULTU, MFLO, MTLO, MFHI, MTHI,
+    SLT, SLTU, LUI,
+    # Logic
+    AND, ANDI, OR, ORI, XOR, XORI, NOR,
+    # Shifts
+    SLL, SLLV, SRA, SRAV, SRL, SRLV,
+    # Memory
+    LW, LB, LBU, LH, LHU, LWL, LWR,
+    SW, SB, SH, SWL, SWR,
+    # Branches
+    BEQ, BNE, BGEZ, BGEZAL, BGTZ, BLEZ, BLTZ, BLTZAL,
+    J, JR, JAL, JALR,
+    # System
+    SYSCALL, BREAK, MFC0, MTC0
   Immediate = distinct uint16   ## An immediate operand.
   Target = distinct range[0 .. (1 shl 26)-1] ## A 26-bit jump target.
+
+func `$`*(x: Immediate): string =
+  fmt"$0x{uint16(x):x}"
+
+func `$`*(x: Target): string =
+  fmt"$0x{word(x):x}"
 
 func absTarget(target: Target, pc: uint32): uint32 =
   ## Resolve a target address.
@@ -202,13 +221,8 @@ func signExt(x: Immediate): uint32 =
   ## Sign extend an immediate to a uint32.
   cast[uint32](int32(cast[int16](uint16(x))))
 
-func `$`*(x: Immediate): string =
-  fmt"$0x{uint16(x):x}"
-
-func `$`*(x: Target): string =
-  fmt"$0x{word(x):x}"
-
 const
+  # The different parts of a machine instruction.
   opcode: BitSlice[int, word] = (pos: 26, width: 6)
   rs: BitSlice[Register, word] = (pos: 21, width: 5)
   rt: BitSlice[Register, word] = (pos: 16, width: 5)
@@ -220,116 +234,141 @@ const
 
 proc decode*(instr: uint32): Opcode {.inline.} =
   ## Decode an instruction to find its opcode.
-  case instr[opcode]
+
+  proc guard(op: Opcode, x: bool): Opcode {.inline.} =
+    ## Return an opcode, but only after checking that a given condition holds.
+    if x:
+      return op
+    else:
+      raise unknownInstructionError
+
+  return case instr[opcode]
   of 0:
     case instr[funct]
-    of 0:
-      if instr[rs] == Register(0): return SLL
-    of 2:
-      if instr[rs] == Register(0): return SRL
-    of 3:
-      if instr[rs] == Register(0): return SRA
-    of 4:
-      if instr[shamt] == 0: return SLLV
-    of 6:
-      if instr[shamt] == 0: return SRLV
-    of 7:
-      if instr[shamt] == 0: return SRAV
-    of 8:
-      if instr[shamt] == 0: return JR
-    of 9:
-      if instr[shamt] == 0: return JALR
-    of 12: return SYSCALL
-    of 13: return BREAK
-    of 16:
-      if instr[shamt] == 0: return MFHI
-    of 17:
-      if instr[shamt] == 0: return MTHI
-    of 18:
-      if instr[shamt] == 0: return MFLO
-    of 19:
-      if instr[shamt] == 0: return MTLO
-    of 24:
-      if instr[shamt] == 0: return MULT
-    of 25:
-      if instr[shamt] == 0: return MULTU
-    of 26:
-      if instr[shamt] == 0: return DIV
-    of 27:
-      if instr[shamt] == 0: return DIVU
-    of 32:
-      if instr[shamt] == 0: return ADD
-    of 33:
-      if instr[shamt] == 0: return ADDU
-    of 34:
-      if instr[shamt] == 0: return SUB
-    of 35:
-      if instr[shamt] == 0: return SUBU
-    of 36:
-      if instr[shamt] == 0: return AND
-    of 37:
-      if instr[shamt] == 0: return OR
-    of 38:
-      if instr[shamt] == 0: return XOR
-    of 39:
-      if instr[shamt] == 0: return NOR
-    of 42:
-      if instr[shamt] == 0: return SLT
-    of 43:
-      if instr[shamt] == 0: return SLTU
-    else: discard
+    of 0: SLL.guard(instr[rs] == r0)
+    of 2: SRL.guard(instr[rs] == r0)
+    of 3: SRA.guard(instr[rs] == r0)
+    of 4: SLLV.guard(instr[shamt] == 0)
+    of 6: SRLV.guard(instr[shamt] == 0)
+    of 7: SRAV.guard(instr[shamt] == 0)
+    of 8: JR.guard(instr[shamt] == 0)
+    of 9: JALR.guard(instr[shamt] == 0)
+    of 12: SYSCALL
+    of 13: BREAK
+    of 16: MFHI.guard(instr[shamt] == 0)
+    of 17: MTHI.guard(instr[shamt] == 0)
+    of 18: MFLO.guard(instr[shamt] == 0)
+    of 19: MTLO.guard(instr[shamt] == 0)
+    of 24: MULT.guard(instr[shamt] == 0)
+    of 25: MULTU.guard(instr[shamt] == 0)
+    of 26: DIV.guard(instr[shamt] == 0)
+    of 27: DIVU.guard(instr[shamt] == 0)
+    of 32: ADD.guard(instr[shamt] == 0)
+    of 33: ADDU.guard(instr[shamt] == 0)
+    of 34: SUB.guard(instr[shamt] == 0)
+    of 35: SUBU.guard(instr[shamt] == 0)
+    of 36: AND.guard(instr[shamt] == 0)
+    of 37: OR.guard(instr[shamt] == 0)
+    of 38: XOR.guard(instr[shamt] == 0)
+    of 39: NOR.guard(instr[shamt] == 0)
+    of 42: SLT.guard(instr[shamt] == 0)
+    of 43: SLTU.guard(instr[shamt] == 0)
+    else: raise unknownInstructionError
   of 1:
     case int(instr[rt])
-    of 0: return BLTZ
-    of 1: return BGEZ
-    of 16: return BLTZAL
-    of 17: return BGEZAL
-    else: discard
-  of 2: return J
-  of 3: return JAL
-  of 4: return BEQ
-  of 5: return BNE
-  of 6:
-    if instr[rt] == Register(0): return BLEZ
-  of 7:
-    if instr[rt] == Register(0): return BGTZ
-  of 8: return ADDI
-  of 9: return ADDIU
-  of 10: return SUBI
-  of 11: return SUBIU
-  of 12: return ANDI
-  of 13: return ORI
-  of 14: return XORI
-  of 15: return LUI
-  of 32: return LB
-  of 33: return LH
-  of 34: return LWL
-  of 35: return LW
-  of 36: return LBU
-  of 37: return LHU
-  of 38: return LWR
-  of 40: return SB
-  of 41: return SH
-  of 42: return SWL
-  of 43: return SW
-  of 46: return SWR
+    of 0: BLTZ
+    of 1: BGEZ
+    of 16: BLTZAL
+    of 17: BGEZAL
+    else: raise unknownInstructionError
+  of 2: J
+  of 3: JAL
+  of 4: BEQ
+  of 5: BNE
+  of 6: BLEZ.guard(instr[rt] == r0)
+  of 7: BGTZ.guard(instr[rt] == r0)
+  of 8: ADDI
+  of 9: ADDIU
+  of 10: SUBI
+  of 11: SUBIU
+  of 12: ANDI
+  of 13: ORI
+  of 14: XORI
+  of 15: LUI
+  of 32: LB
+  of 33: LH
+  of 34: LWL
+  of 35: LW
+  of 36: LBU
+  of 37: LHU
+  of 38: LWR
+  of 40: SB
+  of 41: SH
+  of 42: SWL
+  of 43: SW
+  of 46: SWR
   of 16:
     case int(instr[rs])
-    of 0: return MFC0
-    of 4: return MTC0
-    else: discard
-  else: discard
+    of 0: MFC0
+    of 4: MTC0
+    else: raise unknownInstructionError
+  else: raise unknownInstructionError
 
-  raise unknownInstructionError
+proc format(instr: uint32): string =
+  let
+    op = decode(instr)
+    rd = instr[rd]
+    rs = instr[rs]
+    rt = instr[rt]
+    target = instr[target]
+    shamt = instr[shamt]
+    imm = instr[imm]
 
-const
-  debug = false
-template log(str: untyped) =
-  if debug:
-    echo str
+  proc args(args: varargs[string, `$`]): string =
+    result = $op
+    for i, arg in args:
+      if i != 0: result &= ","
+      result &= " " & arg
 
-proc execute(cpu: var CPU, op: Opcode, instr: uint32) =
+  let
+    r = args(rd, rs, rt)
+    s = args(rd, rs, shamt)
+    i = args(rt, rs, imm)
+    it = args(rt, imm)
+    iss = args(rs, imm)
+    rst = args(rs, rt)
+    rdd = args(rd)
+    rss = args(rs)
+    rds = args(rd, rs)
+    shift = args(rd, rt, shamt)
+    j = args(target)
+    mem = args(rt, fmt"{imm}({rs})")
+    whole = args(fmt"$0x{instr and 0x3ffffff:x}")
+    mfc0 = args(rt, CoRegister(rd))
+    mtc0 = args(CoRegister(rd), rt)
+
+  let
+    kinds: array[Opcode, string] =
+      [ADD: r, ADDI: i, ADDU: r, ADDIU: i, SUB: r, SUBU: i, SUBI: r, SUBIU: i,
+       DIV: rst, DIVU: rst, MULT: rst, MULTU: rst, MFLO: rdd, MTLO: rss,
+       MFHI: rdd, MTHI: rss, SLT: r, SLTU: r, LUI: it,
+       AND: r, ANDI: i, OR: r, ORI: i, XOR: r, XORI: i, NOR: r,
+       SLL: shift, SLLV: r, SRA: shift, SRAV: r, SRL: shift, SRLV: r,
+       LW: mem, LB: mem, LBU: mem, LH: mem, LHU: mem, LWL: mem, LWR: mem,
+       SW: mem, SB: mem, SH: mem, SWL: mem, SWR: mem,
+       BEQ: i, BNE: i, BGEZ: iss, BGEZAL: iss, BGTZ: iss, BLEZ: iss,
+       BLTZ: iss, BLTZAL: iss,
+       J: j, JR: rss, JAL: j, JALR: rds,
+       SYSCALL: whole, BREAK: whole, MFC0: mfc0, MTC0: mtc0]
+
+  for val in kinds:
+      assert val != ""
+
+  return kinds[op]
+
+proc execute(cpu: var CPU, instr: uint32) =
   var newPC = cpu.nextPC + 4
+  let op = decode(instr)
   let rd = instr[rd]
   let rs = instr[rs]
   let rt = instr[rt]
@@ -338,23 +377,12 @@ proc execute(cpu: var CPU, op: Opcode, instr: uint32) =
   let imm = instr[imm]
 
   case op
-  of OR:
-    log fmt"OR {rd}, {rs}, {rt}"
-    cpu[rd] = cpu[rs] or cpu[rt]
-  of AND:
-    log fmt"AND {rd}, {rs}, {rt}"
-    cpu[rd] = cpu[rs] and cpu[rt]
-  of SLTU:
-    log fmt"SLTU {rd}, {rs}, {rt}"
-    cpu[rd] = if cpu[rs] < cpu[rt]: 1 else: 0
-  of SLT:
-    log fmt"SLT {rd}, {rs}, {rt}"
-    cpu[rd] = word(cpu[rs].signed < cpu[rt].signed)
-  of ADDU:
-    log fmt"ADDU {rd}, {rs}, {rt}"
-    cpu[rd] = cpu[rs] + cpu[rt]
+  of OR: cpu[rd] = cpu[rs] or cpu[rt]
+  of AND: cpu[rd] = cpu[rs] and cpu[rt]
+  of SLTU: cpu[rd] = if cpu[rs] < cpu[rt]: 1 else: 0
+  of SLT: cpu[rd] = word(cpu[rs].signed < cpu[rt].signed)
+  of ADDU: cpu[rd] = cpu[rs] + cpu[rt]
   of ADD:
-    log fmt"ADD {rd}, {rs}, {rt}"
     let
       src1 = cpu[rs].signed
       src2 = cpu[rt].signed
@@ -363,35 +391,16 @@ proc execute(cpu: var CPU, op: Opcode, instr: uint32) =
     if src1 < 0 and src2 < low(int32) - src1:
       raise overflowError
     cpu[rd] = src1.unsigned + src2.unsigned
-  of SUBU:
-    log fmt"SUBU {rd}, {rs}, {rt}"
-    cpu[rd] = cpu[rs] - cpu[rt]
-  of SUBIU:
-    log fmt"SUBIU {rt}, {rs}, {imm}"
-    cpu[rt] = cpu[rs] - imm.signExt
-  of JR:
-    log fmt"JR {rs}"
-    newPC = cpu[rs]
-  of SLL:
-    log fmt"SLL {rd}, {rs}, {shamt}"
-    cpu[rd] = cpu[rs] shl shamt
-  of SRL:
-    log fmt"SRL {rd}, {rs}, {shamt}"
-    cpu[rd] = cpu[rs] shr shamt
-  of SRA:
-    log fmt"SRA {rd}, {rs}, {shamt}"
-    cpu[rd] = (cpu[rs].signed shr shamt).unsigned
-  of LUI:
-    log fmt"LUI {rt}, {imm}"
-    cpu[rt] = imm.zeroExt shl 16
-  of ORI:
-    log fmt"ORI {rt}, {rs}, {imm}"
-    cpu[rt] = cpu[rs] or imm.zeroExt
-  of ADDIU:
-    log fmt"ADDIU {rt}, {rs}, {imm}"
-    cpu[rt] = cpu[rs] + imm.signExt
+  of SUBU: cpu[rd] = cpu[rs] - cpu[rt]
+  of SUBIU: cpu[rt] = cpu[rs] - imm.signExt
+  of JR: newPC = cpu[rs]
+  of SLL: cpu[rd] = cpu[rs] shl shamt
+  of SRL: cpu[rd] = cpu[rs] shr shamt
+  of SRA: cpu[rd] = (cpu[rs].signed shr shamt).unsigned
+  of LUI: cpu[rt] = imm.zeroExt shl 16
+  of ORI: cpu[rt] = cpu[rs] or imm.zeroExt
+  of ADDIU: cpu[rt] = cpu[rs] + imm.signExt
   of ADDI:
-    log fmt"ADDI {rt}, {rs}, {imm}"
     let
       src1 = cpu[rs].signed
       src2 = imm.signExt.signed
@@ -401,7 +410,6 @@ proc execute(cpu: var CPU, op: Opcode, instr: uint32) =
       raise overflowError
     cpu[rt] = src1.unsigned + src2.unsigned
   of SUBI:
-    log fmt"SUBI {rt}, {rs}, {imm}"
     let
       src1 = cpu[rs].signed
       src2 = imm.signExt.signed
@@ -410,77 +418,45 @@ proc execute(cpu: var CPU, op: Opcode, instr: uint32) =
     if src1 < 0 and src2 > high(int32) + src1:
       raise overflowError
     cpu[rt] = src1.unsigned - src2.unsigned
-  of SW:
-    log fmt"SW {rt}, {rs}, {imm}"
-    cpu.write[:uint32](cpu[rs] + imm.signExt, cpu[rt])
-  of LW:
-    log fmt"LW {rt}, {rs}, {imm}"
-    cpu[rt] = cpu.read[:uint32](cpu[rs] + imm.signExt)
+  of SW: cpu.write[:uint32](cpu[rs] + imm.signExt, cpu[rt])
+  of LW: cpu[rt] = cpu.read[:uint32](cpu[rs] + imm.signExt)
   of BNE:
-    log fmt"BNE {rs}, {rt}, {imm}"
     if cpu[rs] != cpu[rt]:
       newPC = cpu.nextPC + imm.signExt shl 2
   of BEQ:
-    log fmt"BEQ {rs}, {rt}, {imm}"
     if cpu[rs] == cpu[rt]:
       newPC = cpu.nextPC + imm.signExt shl 2
   of BLEZ:
-    log fmt"BLEZ {rs}, {imm}"
     if cpu[rs].signed <= 0:
       newPC = cpu.nextPC + imm.signExt shl 2
   of BGTZ:
-    log fmt"BGTZ {rs}, {imm}"
     if cpu[rs].signed > 0:
       newPC = cpu.nextPC + imm.signExt shl 2
-  of SH:
-    log fmt"SH {rt}, {rs}, {imm}"
-    cpu.write[:uint16](cpu[rs] + imm.signExt, cast[uint16](cpu[rt]))
-  of SB:
-    log fmt"SB {rt}, {rs}, {imm}"
-    cpu.write[:uint8](cpu[rs] + imm.signExt, cast[uint8](cpu[rt]))
-  of ANDI:
-    log fmt"ANDI {rt}, {rs}, {imm}"
-    cpu[rt] = cpu[rs] and imm.zeroExt
-  of LB:
-    log fmt"LB {rt}, {rs}, {imm}"
-    cpu[rt] = int32(cpu.read[:int8](cpu[rs] + imm.signExt)).unsigned
-  of LBU:
-    log fmt"LBU {rt}, {rs}, {imm}"
-    cpu[rt] = uint32(cpu.read[:uint8](cpu[rs] + imm.signExt))
-  of LH:
-    log fmt"LH {rt}, {rs}, {imm}"
-    cpu[rt] = int32(cpu.read[:int16](cpu[rs] + imm.signExt)).unsigned
-  of LHU:
-    log fmt"LHU {rt}, {rs}, {imm}"
-    cpu[rt] = uint32(cpu.read[:uint16](cpu[rs] + imm.signExt))
-  of J:
-    log fmt"J {target}"
-    newPC = target.absTarget(cpu.nextPC)
+  of SH: cpu.write[:uint16](cpu[rs] + imm.signExt, cast[uint16](cpu[rt]))
+  of SB: cpu.write[:uint8](cpu[rs] + imm.signExt, cast[uint8](cpu[rt]))
+  of ANDI: cpu[rt] = cpu[rs] and imm.zeroExt
+  of LB: cpu[rt] = int32(cpu.read[:int8](cpu[rs] + imm.signExt)).unsigned
+  of LBU: cpu[rt] = uint32(cpu.read[:uint8](cpu[rs] + imm.signExt))
+  of LH: cpu[rt] = int32(cpu.read[:int16](cpu[rs] + imm.signExt)).unsigned
+  of LHU: cpu[rt] = uint32(cpu.read[:uint16](cpu[rs] + imm.signExt))
+  of J: newPC = target.absTarget(cpu.nextPC)
   of JALR:
-    log fmt"JALR {rs}"
     newPC = cpu[rs]
     cpu[Register(31)] = cpu.nextPC + 4
   of JAL:
-    log fmt"JAL {target}"
     newPC = target.absTarget(cpu.nextPC)
     cpu[Register(31)] = cpu.nextPC + 4
-  of MTC0:
-    log fmt "MTC0 {CoRegister(rd)}, {rt}"
-    cpu.cop0[CoRegister(rd)] = cpu[rt]
-  of MFC0:
-    log fmt "MFC0 {rt}, {CoRegister(rd)}"
-    cpu[rt] = cpu.cop0[CoRegister(rd)]
+  of MTC0: cpu.cop0[CoRegister(rd)] = cpu[rt]
+  of MFC0: cpu[rt] = cpu.cop0[CoRegister(rd)]
   else:
-    log fmt"Unknown opcode {op}"
     raise unknownInstructionError
 
   cpu.pc = cpu.nextPC
   cpu.nextPC = newPC
 
 proc step(cpu: var CPU) =
-  let instr = cpu.fetch()
   # The execute function is in charge of updating pc and nextPC.
-  cpu.execute(decode(instr), instr)
+  cpu.execute(cpu.fetch)
 
 var clocks = 0
 
